3b02ea4a7edd88ed57a19ecf56d7ea19b5c0d986
Your Name <you@example.com>
2024-09-30 18:56:18 +0200

    Change the name of the app

--- src/App.java
+++ src/App.java
@@ -2,7 +2,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
 
-        frame = new JFrame("Bank Application");
+        frame = new JFrame("Todolist APP");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
         frame.setSize(1000, 200); 
         frame.setLayout(new BorderLayout())
