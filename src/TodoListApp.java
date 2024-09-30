0051e82ab31945cf13e1a09f6982889f4048b472
Your Name <you@example.com>
2024-09-30 19:19:39 +0200

    Edit bugs new

--- src/TodoListApp.java
+++ src/TodoListApp.java
@@ -4,22 +4,23 @@ import java.awt.*;
 public class TodoListApp {
     private JFrame frame;
 
-    private TodoListApp() {
-      
-        frame = new JFrame("TodolistAPP");
+    // Constructor
+    public TodoListApp() {
+        setupGUI();
+    }
+
+    private void setupGUI() {
+        frame = new JFrame("Todolist APP");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(480, 280);
         frame.setLayout(new BorderLayout());
-
-       
+        
+        // Make the frame visible
         frame.setVisible(true);
     }
+
     public static void main(String[] args) {
-      
+        // Create an instance of TodoListApp
         new TodoListApp();
-     
     }
-
-    
-       
-
+}
