e65c85d65c18d1651654affd2b0d9d2322bbb443
Your Name <you@example.com>
2024-09-30 18:44:48 +0200

    Close application on window close

--- src/App.java
+++ src/App.java
@@ -1,5 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-    }
+
+        frame = new JFrame("Bank Application");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+       
 }
