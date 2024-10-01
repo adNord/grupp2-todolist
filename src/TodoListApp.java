e4b65495629d477a690f43ba1b28267e10b0c56d
Azadan <azadan@live.com>
2024-10-01 19:17:55 +0200

    add task method

--- src/TodoListApp.java
+++ src/TodoListApp.java
@@ -1,9 +1,12 @@
-import javax.swing.*;
 import java.awt.*;
+import javax.swing.*;
+
 
 public class TodoListApp {
     private JFrame frame;
     private JPanel topPanel;
+    private JPanel taskListContainer = new JPanel();
+    private JPanel taskBox = new JPanel();
 
     // Constructor
     public TodoListApp() {
@@ -13,11 +16,11 @@ public class TodoListApp {
     private void setupGUI() {
         frame = new JFrame("Todolist APP");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        //frame.setSize(480, 280);
+        frame.setSize(480, 480);
         frame.setLayout(new BorderLayout());
         
         // Make the frame visible
-        frame.setVisible(true);
+        
 
         topPanel = new JPanel();
         frame.add(topPanel, BorderLayout.NORTH);
@@ -25,15 +28,42 @@ public class TodoListApp {
         JTextField inputTaskText = new JTextField(20);
         topPanel.add(inputTaskText);
 
+        taskBox.setLayout(new BorderLayout());
+
+        taskListContainer.setLayout(new BoxLayout(taskListContainer, BoxLayout.Y_AXIS));
+        //taskListContainer.setSize(200, 200);
+        taskBox.add(taskListContainer, BorderLayout.NORTH);
+        frame.add(taskBox, BorderLayout.CENTER);
+
+
         JButton addTaskButton = new JButton("Add Task");
         topPanel.add(addTaskButton);
+        addTaskButton.addActionListener(e -> {
+            addTask(inputTaskText.getText());
+            inputTaskText.setText("");
+        });
         
 
-        JPanel taskListContainer = new JPanel();
-        taskListContainer.setSize(200, 200);
-        taskListContainer.setBackground(Color.BLUE);
-        frame.add(taskListContainer, BorderLayout.CENTER);
-        frame.pack();
+       
+        //frame.pack();
+        frame.setVisible(true);
+    }
+
+    private void addTask(String taskText) {
+        if (!taskText.isEmpty()) {
+            JPanel taskPanel = new JPanel();
+            taskPanel.setLayout(new BorderLayout());
+            //taskPanel.setPreferredSize(new Dimension(380,30));
+            JLabel taskLabel = new JLabel(taskText);
+            JCheckBox checkBox = new JCheckBox();
+            taskPanel.add(taskLabel, BorderLayout.CENTER);
+            taskPanel.add(checkBox, BorderLayout.WEST);
+            taskListContainer.add(taskPanel);
+            taskListContainer.revalidate();
+            taskListContainer.repaint();
+            
+
+        }
     }
 
 
